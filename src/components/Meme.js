14bceacdf99e760120eb22d88189f7f6971eebf6
Liman <liman123shen@gmail.com>
2022-07-15 15:27:40 -0400

    add image to meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,20 @@
 import memesData from "../memesData";
+import {useState} from 'react';
 
 export default function Meme () {
 
+    const [memeImage = '', setMemeUrl] = useState()
+    
     function getRandomMeme () {
         const memesArr = memesData.data.memes
         // Use Math.random() function to get the random number between(0-1, 1 exclusive).
         // Multiply it by the array length to get the numbers between(0-arrayLength).
         // Use Math.floor() to get the index ranging from(0 to arrayLength-1).
         const randomMeme = memesArr[Math.floor(Math.random() * memesArr.length)]
-        console.log(randomMeme.url)
+        // console.log(randomMeme.url)
+        setMemeUrl(randomMeme.url)
     }
+
 
     return (
         <main>
@@ -18,6 +23,7 @@ export default function Meme () {
                 <input autoComplete='off' placeholder="Text on bottom" type='text'></input>
                 <button type='button' onClick={getRandomMeme}>Get a new meme image 🖼</button>
             </form>
+            <img src={memeImage} className='meme-image'/>
         </main>
     )
 }
