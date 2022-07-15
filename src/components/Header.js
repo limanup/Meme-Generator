44ff7d445db62b35cf4c2062cbb922c04b956acd
Liman <liman123shen@gmail.com>
2022-07-14 21:33:38 -0400

    meme generator header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <img src='images/troll-face.png' className="header-img"/>
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    )
+}
