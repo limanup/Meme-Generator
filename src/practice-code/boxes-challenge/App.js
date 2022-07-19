c2bcbf6a390423ffca755119425c3554e88a906f
Liman <liman123shen@gmail.com>
2022-07-18 23:14:51 -0400

    box challenge - dynamic styles

--- src/practice-code/boxes-challenge/App.js
+++ src/practice-code/boxes-challenge/App.js
@@ -1,7 +1,7 @@
-import React from "react"
-import boxes from "./boxes"
+import React from "react";
+import boxes from "./boxes";
 
-export default function App() {
+export default function App(props) {
     /**
      * Challenge part 1:
      * 1. Initialize state with the default value of the
@@ -10,17 +10,24 @@ export default function App() {
      *    as an empty square (black border, transparent bg color)
      *    (Don't worry about using the "on" property yet)
      */
-    
-    const [arrBoxes, setArrBoxes] = React.useState(boxes)
-    
-    const squares = arrBoxes.map(box => (
-    <div className='boxes' key={box.id}></div>
-    ))
-    
+
+    const [arrBoxes, setArrBoxes] = React.useState(boxes);
+
+    // const backColor = props.darkMode? "#222222" : "#CCCCCC"
+    // const styles = {backgroundColor: backColor}
+
+    const squares = arrBoxes.map((box) => (
+        <div
+            className="boxes"
+            key={box.id}
+            styles={{ backgroundColor: props.darkMode ? "#222222" : "#CCCCCC" }}
+        ></div>
+    ));
+
     return (
         <main>
             <h1>Boxes will go here</h1>
             {squares}
         </main>
-    )
+    );
 }
