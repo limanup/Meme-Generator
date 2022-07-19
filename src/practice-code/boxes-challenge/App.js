2d813d7280a5722e72be16f7feb3c24d6515af00
Liman <liman123shen@gmail.com>
2022-07-18 23:05:29 -0400

    box challenge - setting up page

---
+++ src/practice-code/boxes-challenge/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import boxes from "./boxes"
+
+export default function App() {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+    
+    const [arrBoxes, setArrBoxes] = React.useState(boxes)
+    
+    const squares = arrBoxes.map(box => (
+    <div className='boxes' key={box.id}></div>
+    ))
+    
+    return (
+        <main>
+            <h1>Boxes will go here</h1>
+            {squares}
+        </main>
+    )
+}
