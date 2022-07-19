ae4cf03741d555008c637bf5c05f560e6516f48c
Liman <liman123shen@gmail.com>
2022-07-19 12:43:59 -0400

    box challenge - unified state - pass function from App.js to component

--- src/practice-code/boxes-challenge/App.js
+++ src/practice-code/boxes-challenge/App.js
@@ -3,35 +3,16 @@ import boxes from "./boxes";
 import Box from "./Box";
 
 export default function App(props) {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from boxes.js
-     * 2. Map over that state array and display each one
-     *    as an empty square (black border, transparent bg color)
-     *    (Don't worry about using the "on" property yet)
-     */
-
     const [arrBoxes, setArrBoxes] = React.useState(boxes);
 
-    // const backColor = props.darkMode? "#222222" : "#CCCCCC"
-    // const styles = {backgroundColor: backColor}
-    
-    /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
-     */
-    
+    function toggle(id) {
+        console.log(id);
+    }
+
     const squares = arrBoxes.map((box) => (
-        <Box on={box.on} key={box.id} />
+        <Box on={box.on} key={box.id} id={box.id} toggle={toggle} />
     ));
-    
+
     return (
         <main>
             <h1>Boxes will go here</h1>
--- src/practice-code/boxes-challenge/Box.js
+++ src/practice-code/boxes-challenge/Box.js
@@ -1,27 +1,12 @@
 import { useState } from "react";
 
 export default function Box(props) {
-    /**
-     * Challenge: Create state controlling whether
-     * this box is "on" or "off". Use the incoming
-     * `props.on` to determine the initial state.
-     *
-     * Create an event listener so when the box is clicked,
-     * it toggles from "on" to "off".
-     *
-     * Goal: clicking each box should toggle it on and off.
-     */
-
-    const [isColorOn, setIsColorOn] = React.useState(props.on);
 
     return (
         <div
             className="boxes"
-            style={{ backgroundColor: isColorOn ? "#222222" : "transparent" }}
-            onClick={() => {
-                setIsColorOn(!isColorOn);
-                // setIsColorOn(prevOn => !prevOn)
-            }}
+            style={{ backgroundColor: props.on ? "#222222" : "transparent" }}
+            onClick={() => props.toggle(props.id)}
         ></div>
     );
 }
