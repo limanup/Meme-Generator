754f140faf561f25ce42e585f58f496850b943b6
Liman <liman123shen@gmail.com>
2022-07-19 00:13:06 -0400

    box challenge - local state

--- src/practice-code/boxes-challenge/Box.js
+++ src/practice-code/boxes-challenge/Box.js
@@ -1,9 +1,27 @@
-export default function Box (props) {
+import { useState } from "react";
+
+export default function Box(props) {
+    /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     *
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     *
+     * Goal: clicking each box should toggle it on and off.
+     */
+
+    const [isColorOn, setIsColorOn] = React.useState(props.on);
+
     return (
         <div
             className="boxes"
-            on={props.on}
-            style={{backgroundColor: props.on ? "#222222" : "none"}}
+            style={{ backgroundColor: isColorOn ? "#222222" : "transparent" }}
+            onClick={() => {
+                setIsColorOn(!isColorOn);
+                // setIsColorOn(prevOn => !prevOn)
+            }}
         ></div>
-    )
+    );
 }
