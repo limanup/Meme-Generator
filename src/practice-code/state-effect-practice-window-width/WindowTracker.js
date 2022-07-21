83716076b6a8b323dbe64ef4f48e24520f9d0986
Liman <liman123shen@gmail.com>
2022-07-21 16:02:53 -0400

    window width practice memory leak

--- src/practice-code/state-effect-practice-window-width/WindowTracker.js
+++ src/practice-code/state-effect-practice-window-width/WindowTracker.js
@@ -1,7 +1,35 @@
 import React from "react"
+import {useState, useEffect} from React
 
 export default function WindowTracker() {
+
+    /**
+     * Challenge:
+     * 1. Create state called `windowWidth`, default to 
+     *    `window.innerWidth`
+     * 2. When the window width changes, update the state
+     * 3. Display the window width in the h1 so it updates
+     *    every time it changes
+     */
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth)
+        })
+    }, [])
+
     return (
-        <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {windowWidth}</h1>
     )
 }
+
+
+/**
+ * above code will generate a memory leak when the window width is toggled off
+ * (in App.js)
+ * Error message:
+ * Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function. at WindowTracker (exe1.bundle.js:69:56)
+ * 
+ */
